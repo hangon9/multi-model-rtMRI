cfdb139df451c3b9f6a9122766c39cd4256ef5f3
kafeigongzai <kafeigongzai@gmail.com>
2026-06-27 20:03:50 +0200

    changed name of wav2vec2_encoder

--- src/models/wav2vec_encoder.py
+++ src/models/wav2vec_encoder.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from transformers import Wav2Vec2Model
 
 
-class FrozenWav2Vec2Encoder(nn.Module):
+class Wav2Vec2Encoder(nn.Module):
     def __init__(
         self,
         model_name="facebook/wav2vec2-base-960h",
