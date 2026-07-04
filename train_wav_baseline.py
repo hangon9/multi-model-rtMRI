066fd0759416e56814bd1108bbe8c2cb47026c88
kafeigongzai <kafeigongzai@gmail.com>
2026-07-04 21:29:16 +0200

    record all lr

--- train_wav_baseline.py
+++ train_wav_baseline.py
@@ -526,7 +526,8 @@ def main():
                 epoch=epoch,
                 phase="training",
                 metrics=train_log,
-                lr=optimizer.param_groups[0]["lr"],
+                lr_encoder=optimizer.param_groups[0]["lr"],
+                lr_downstream=optimizer.param_groups[2]["lr"],
                 classification_task=classification_task,
                 log_to_console=False,
             )
